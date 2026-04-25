d00bfde73e6895d268b5bad0f9d57e4d347b178f
Kauã Alves <kauaalvesnascimento22@gmail.com>
2026-04-25 14:52:56 -0300

    feat: simplify order send message

--- Order.Input/Domain/Messages/OrderMessage.cs
+++ Order.Input/Domain/Messages/OrderMessage.cs
@@ -2,19 +2,8 @@ using Order.Input.Domain.Entities;
 
 namespace Order.Input.Domain.Messages;
 
-public readonly record struct OrderItemMessage(Guid ProductId, int Quantity)
-{
-    public OrderItemMessage(OrderItemEntity orderItem) : this(orderItem.ProductId, orderItem.Quantity) {}
-};
-
-public readonly record struct OrderMessage(Guid OrderId, Guid CustomerId, decimal TotalAmount, OrderItemMessage[] Items)
+public readonly record struct OrderMessage(Guid OrderId)
 {
     public OrderMessage(OrderEntity order) 
-        : this(
-            order.OrderId,
-            order.CustomerId,
-            order.TotalAmount,
-            order.Items?
-                .Select(x => new OrderItemMessage(x))
-                .ToArray() ?? []) { }
+        : this(order.OrderId) { }
 }
